e0b307cb0fd3294484253d5d858652fd7b6b604a
Colin <silvercpb@gmail.com>
2017-08-30 15:33:56 -0400

    Added ssome more CSS

--- components/series.js
+++ components/series.js
@@ -3,12 +3,12 @@ Vue.component('series', {
 	props: ['series', 'seriesTitle', 'seriesDescription', 'seriesAuthor', 'seriesStatements', 'addStatement', 'statementInput', 'incScore', 'decScore', 'hideStatements', 'toggleStatementsVisible'],
 	template: `
 						<div class="row">
-							<div class="col-md-12">
-								<h2>{{ seriesTitle }} by {{ seriesAuthor }}</h2>
-								<p>{{ seriesDescription }}</p>
-								<input v-model="statementInput" ></input>
-								<input type="submit" value="Add Statement" v-on:click="addStatement(series, statementInput, 'Author')">
-								<input type="submit" value="Show Statements" v-on:click="toggleStatementsVisible(series)">
+							<div class="col-md-12 series">
+									<h2>{{ seriesTitle }} by {{ seriesAuthor }}</h2>
+									<p>{{ seriesDescription }}</p>
+									<input v-model="statementInput" ></input>
+									<input type="submit" value="Add Statement" v-on:click="addStatement(series, statementInput, 'Author')">
+									<input type="submit" value="Show Statements" v-on:click="toggleStatementsVisible(series)">
 								<statement
 								v-if="!hideStatements"
 								v-for="statement in seriesStatements"
